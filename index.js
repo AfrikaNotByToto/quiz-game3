cd65a4d5fe4e4dff7a14a14de294b6c09e523e17
Tanya <alisaeva4498@mail.ru>
2022-12-02 15:00:50 +0300

    chalk

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const rl = require('readline-sync');
 const fs = require('fs');
-// const chalk = require('chalk');
+const chalk = require('chalk');
 
 function getQuestions() {
   const arr = [];
   const files = fs.readdirSync('./topics', 'utf-8');
-  for (const file of files) {
+  for(const file of files) {
     const readfiles = fs.readFileSync(`./topics/${file}`, 'utf-8');
     arr.push(readfiles.split('\n').filter((str) => str !== ''));
   }
@@ -14,9 +14,9 @@ function getQuestions() {
 
 console.log('Добро пожаловать на  Квиз!\n');
 const answer = rl.question('Как тебя зовут?\n');
-console.log(`Привет, ${answer}! Давай сыграем в игру!\n`);
+console.log(`Привет, ${chalk.blue(answer)}! Давай сыграем в игру!🪚\n`);
 
-console.log('Выбери тему:');
+console.log(chalk.bgCyanBright('Выбери тему:'));
 console.log('1. GTA V');
 console.log('2. Сумерки');
 console.log('3. Теория Большого Взрыва\n');
@@ -24,17 +24,17 @@ quize();
 
 function quize() {
   let score = 0;
-  let answer = rl.question('Введи номер темы:');
+  let answer = rl.question(chalk.bgCyanBright('Введи номер темы:'));
   if (answer < 4 && answer > 0) {
     const questions = getQuestions();
     const quizTopic = questions[answer - 1];
     for (let i = 1; i < quizTopic.length; i += 2) {
-      console.log(quizTopic[i - 1]);
+      console.log(chalk.blue(quizTopic[i - 1]));
       const answer = rl.question('Твой ответ: ');
       if (answer.toLowerCase() === quizTopic[i].toLowerCase()) {
-        console.log('Верно!');
+        console.log(chalk.magenta('Верно!'));
         score += 1;
-      } else console.log('Ты не прав!');
+      } else console.log(chalk.red('Ты не прав!'));
     }
     console.log(`Твой результат: ${score}`);
   } else {
