146c7b36085d0bdf4096989a815bc9589df7b1b1
Slimshady3133 <i.puzakov2010@mail.ru>
2022-12-02 11:11:51 +0300

    sozdal massiv

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function massiv() {
+  const arr = [];
+  const first = fs.readFileSync(
+    './topics/nighthawk_flashcard_data.txt',
+    'utf-8'
+  );
+  const arrArray = first.split('\n').filter((str) => str !== '');
+  arr.push(arrArray);
+  return arr;
+}
+console.log(massiv());
