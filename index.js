e120c258f0390e9c6d315b7d66f0603eceb6b71a
Tanya <alisaeva4498@mail.ru>
2022-12-02 14:21:45 +0300

    score done

--- index.js
+++ index.js
@@ -11,26 +11,33 @@ function getQuestions() {
   return arr;
 }
 
-
 console.log("Добро пожаловать на  Квиз!\n");
 const answer = rl.question("Как тебя зовут?\n");
 console.log(`Привет, ${answer}! Давай сыграем в игру!\n`);
 
 console.log("Выбери тему:");
-console.log("1. Сумерки");
-console.log("2. GTA V");
+console.log("1. GTA V");
+console.log("2. Сумерки");
 console.log("3. Теория Большого Взрыва\n");
 quize();
 
 function quize() {
+  let score = 0;
   let answer = rl.question("Введи номер темы:");
-  const questions = getQuestions();
-  const quizTopic = questions[answer - 1];
-  for (let i = 1; i < quizTopic.length; i += 2) {
-    console.log(quizTopic[i - 1]);
-    const answer = rl.question("Твой ответ: ");
-    if (answer.toLowerCase() === quizTopic[i].toLowerCase())
-      console.log("Верно!");
-    else console.log("Ты не прав!");
+  if (answer < 4 && answer > 0) {
+    const questions = getQuestions();
+    const quizTopic = questions[answer - 1];
+    for (let i = 1; i < quizTopic.length; i += 2) {
+      console.log(quizTopic[i - 1]);
+      const answer = rl.question("Твой ответ: ");
+      if (answer.toLowerCase() === quizTopic[i].toLowerCase()) {
+        console.log("Верно!");
+        score += 1;
+      } else console.log("Ты не прав!");
+    }
+    console.log(`Твой результат: ${score}`);
+  } else {
+    console.log("Такой темы нет. Выбери тему");
+    quize();
   }
 }
